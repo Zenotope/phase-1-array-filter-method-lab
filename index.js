ae587258ae2a47bff8aafaefa2d41ef944172787
zaak <zaak.kerstetter@gmail.com>
2022-05-17 19:32:44 -0600

    commit tree 7365884b2ba63578cd444f66dc3d28e518cf59e1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, search){
+    return drivers.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === search.toLowerCase()
+    )
+}
+
+function fuzzyMatch(names, queary){
+    return names.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(queary.toLowerCase()) === 0
+    )
+}
+
+function matchName(name, hometown){
+    return name.filter(
+        (possibleMatch) => possibleMatch.name === hometown
+    )
+}
